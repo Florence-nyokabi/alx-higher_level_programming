1a6d0ddc979c9ca8f57aeca03a7f9c9d191aa825
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-05-31 21:57:42 +0300

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
         if i % 3 == 0:
             print("Fizz", end='')
         if i % 5 == 0:
-             print("Buzz", end='')
+            print("Buzz", end='')
         if i % 3 and i % 5:
-             print("{:d}".format(i), end='')
+            print("{:d}".format(i), end='')
         print(end=' ')
