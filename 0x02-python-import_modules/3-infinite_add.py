4a81d8ac7f10325d20dd7c3ba9ac17deaa607943
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-06-02 12:18:10 +0300

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+from sys import argv
+
+
+if __name__ == "__main__":
+    newsum = 0
+    if len(argv) > 1:
+        for i in range(1, len(argv)):
+            newsum += int(argv[i])
+    print(newsum)
