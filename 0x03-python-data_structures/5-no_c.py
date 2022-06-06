12059f369e82ad1269f07709054b527e6da09e43
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-06-06 10:41:36 +0300

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for c in my_string:
-        if c != 'c' and c != 'C':
-            new_string += c
-            return new_string
+    str = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            str = str + i
+    return str
