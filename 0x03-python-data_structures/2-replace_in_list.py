dcb25247213dc79788297afd58570eb244e9d502
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-06-06 10:43:30 +0300

    Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if 0 <= idx < len(my_list):
+    if idx > (len(my_list) - 1) or idx < 0:
+        return my_list
+    else:
         my_list[idx] = element
-        return (my_list)
+        return my_list
