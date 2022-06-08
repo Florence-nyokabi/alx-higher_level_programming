450a1b9f9a59029fc959102b0963034d058e9c96
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-06-08 20:00:30 +0300

    Delete by value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,4 +3,4 @@ def complex_delete(a_dictionary, value):
     for idx in list(a_dictionary.keys()):
         if a_dictionary[idx] == value:
             del a_dictionary[idx]
-            return a_dictionary
+    return a_dictionary
