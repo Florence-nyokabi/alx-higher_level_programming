33240539df02a9ff70a0b2c16c0a2cc1799ae597
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-06-20 12:44:05 +0300

    Integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        res = a / b
-    except:
-        res = None
+        math = a / b
+    except ZeroDivisionError:
+        math = None
     finally:
-        print("Inside result: {}".format(res))
-    return res
+        print("Inside result: {}".format(math))
+        return (math)
