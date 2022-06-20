fbba04045b00a0fac1a0fc8a968a87bf41af5e29
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-06-20 15:14:16 +0300

    ByteCode -> Python #4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,9 +4,9 @@ def magic_calculation(a, b):
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception('too far')
+                raise Exception('Too far')
             else:
-                result += (a ** b) / i
+                result += a ** b / i
         except:
             result = a + b
             break
