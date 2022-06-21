240feae8c1a06407380417ad6df2aa50616215f4
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-06-21 13:52:21 +0300

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """__(int): size of a side"""
+    def __init__(self, size):
+        """initializes a square
+
+        Args:
+            size(int):size of a side
+
+        Return: None
+        """
+        self.__size = size
