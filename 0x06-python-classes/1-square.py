178e28de8868f9d1718205e984c9f550773801b9
Florence-wangui <wanguif018@gmail.com>
2022-06-21 03:48:53 -0700

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Square class"""
 
+
 class Square:
     """__(int): size of a side"""
     def __init__(self, size):
         """initializes a square
 
         Args:
-        size(int):size of a side
+            size(int):size of a side
 
         Return: None
         """
