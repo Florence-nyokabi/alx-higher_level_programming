014dd260a37ad1bf003825b86dada23e0d02e6f6
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-06-28 13:52:24 +0300

    #pythonic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l += ["BestSchool"]
-    return ", ".join(l)
+def magic_string():
+    setattr(magic_string, "x", getattr(magic_string, "x", -1) + 1)
+    return "Holberton" + ", Holberton" * getattr(magic_string, "x", 0)
