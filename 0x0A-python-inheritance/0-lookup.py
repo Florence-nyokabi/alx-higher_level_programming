ad446925e69e534e46de4f2bb9b9a6a28e3bd13f
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-07-04 12:20:24 +0300

    Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-It contains the lookup function
+Contains the lookup function
 """
 
+
 def lookup(obj):
-    """returns the list of available attributes and methods of an object"""
+    """returns a list of available attributes and methods of an object"""
     return dir(obj)
