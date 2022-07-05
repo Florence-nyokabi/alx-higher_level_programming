d55f792b5eddd0e58eadbe878826b3449d7331ef
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-07-05 17:16:29 +0300

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-Contains the read_file module
+Contains the read_file function
 """
 
+
 def read_file(filename=""):
-    """reads a text file (UTF8) and prints it to stdout"""
-    with open('workfile', encoding="utf-8") as f:
-        read_data = f.read()
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
