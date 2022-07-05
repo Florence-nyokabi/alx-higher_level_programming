5a26c5c847052262302ae3463c11a46053ae2f28
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-07-05 18:16:59 +0300

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,40 +1,31 @@
 #!/usr/bin/python3
-"""
-reads stdin line by line and computes metrics
-"""
-import sys
 
-file_size = 0
-status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
-i = 0
-try:
-    for line in sys.stdin:
-        tokens = line.split()
-        if len(tokens) >= 2:
-            a = i
-            if tokens[-2] in status_tally:
-                status_tally[tokens[-2]] += 1
-                i += 1
-            try:
-                file_size += int(tokens[-1])
-                if a == i:
-                    i += 1
-            except:
-                if a == i:
-                    continue
-        if i % 10 == 0:
-            print("File size: {:d}".format(file_size))
-            for key, value in sorted(status_tally.items()):
-                if value:
-                    print("{:s}: {:d}".format(key, value))
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print("{:s}: {:d}".format(key, value))
+if __name__ == "__main__":
 
-except KeyboardInterrupt:
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print("{:s}: {:d}".format(key, value))
+    import sys
+
+    size = 0
+    statuscd = {}
+
+    while True:
+        try:
+            counter = 0
+            for line in sys.stdin:
+                line = line.split()
+
+                size += int(line[8])
+                if statuscd.get(line[7], -1) == -1:
+                    statuscd[line[7]] = 1
+                else:
+                    statuscd[line[7]] += 1
+
+                if counter == 10:
+                    break
+                counter += 1
+
+        except KeyboardInterrupt:
+            exit()
+        finally:
+            print("File size: {}".format(size))
+            for key in sorted(statuscd):
+                print("{}: {}".format(key, statuscd[key]))
