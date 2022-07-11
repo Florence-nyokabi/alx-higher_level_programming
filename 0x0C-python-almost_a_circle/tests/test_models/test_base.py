c3385aa6c3360b74844cdac09b3411522ea9c363
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-07-11 21:37:09 +0300

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines unittests for base.py.
+"""Defines unittests for base.py."""
 Unittest classes:
     TestBase_instantiation - line 23
     TestBase_to_json_string - line 110
