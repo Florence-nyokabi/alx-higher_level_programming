82f5dd333236aa16689939c73d71e17d0de0221a
Florence-wangui <wanguif018@gmail.com>
2022-08-09 02:25:09 -0700

    The database name will be passed as argument of mysql command (in the following example: mysql is the name of the database)

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- Write a script that lists all the tables of a database in your MySQL server.
+-- The database name will be passed as argument of mysql command (in the following example: mysql is the name of the database)
+
+SHOW TABLES;
