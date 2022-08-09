d79f818d1f801040ccf49caa2ed2c46feabc915b
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-08-09 16:36:20 +0300

    Create 16-no_link.sql

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,9 @@
+-- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+
+-- Don’t list rows without a name value
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+-- The database name will be passed as an argument to the mysql command
+-- In this example, new data have been added to the table second_table.
+
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
