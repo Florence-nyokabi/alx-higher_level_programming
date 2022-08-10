395d57a4e757d2619f0c85b0a90e5ba946569b78
Florence-wangui <wanguif018@gmail.com>
2022-08-10 00:27:57 -0700

    name VARCHAR(256)

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,8 @@
+-- Write a script that creates the table id_not_null on your MySQL server.
+-- id_not_null description:
+-- id INT with the default value 1
+-- name VARCHAR(256)
+-- The database name will be passed as an argument of the mysql command
+-- If the table id_not_null already exists, your script should not fail
+
+CREATE TABLE IF NOT EXISTS id_not_null( id INT default 1, name VARCHAR(256));
