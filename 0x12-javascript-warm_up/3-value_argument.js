5a4adce4d828788d4c3201d905482fe75c7998da
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-05 12:53:17 +0300

    Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 2) {
+let argsLen = 0;
+process.argv.forEach((element) => { argsLen++; });
+if (argsLen === 2) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
