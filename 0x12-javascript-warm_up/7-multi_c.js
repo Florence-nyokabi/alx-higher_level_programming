38590f6337a868c431309443dbea45dbf8cd0563
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-05 13:24:51 +0300

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-let x = process.argv[2];
-x = parseInt(x);
-if (isNaN(x)) {
-  console.log('Missing number of occurences');
-} else {
-  for (let i = 0; i < x; i++) {
-    console.log('C is fun');
-  }
+
+const arg = parseInt(process.argv[2], 10);
+
+if (isNaN(arg)) {
+  console.log('Missing number of occurrences');
+}
+
+for (let i = 1; i <= arg; i++) {
+  console.log('C is fun');
 }
