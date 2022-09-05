6370d7d83c66ef24ef25ae31027c6689b29532df
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-05 11:08:07 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-if (process.argv.length <= 2) {
-	console.log('No arguement');
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-	if (process.argv.length > 3) {
-		console.log('Arguements found');
-		} else {
-			console.log('Argument found');
-		}
+  console.log('Arguments found');
 }
