b0e627bb3e1f53de8c28fb205a6e17f6dc5c8923
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-06 15:02:28 +0300

    Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,20 +1,34 @@
 #!/usr/bin/node
 
-class Rectangle {
+const Rectangle = class {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+    if (w <= 0 || h <= 0) {
+      w = undefined;
+      h = undefined;
+    }
+    if (w !== undefined && h !== undefined) {
       this.width = w;
       this.height = h;
     }
   }
 
   print () {
-    for (let i = 1; i <= this.height; i++) {
-      let row = '';
-      for (let j = 1; j <= this.width; j++) row += 'X';
-      console.log(row);
+    const printer = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(printer);
     }
   }
-}
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
 
 module.exports = Rectangle;
