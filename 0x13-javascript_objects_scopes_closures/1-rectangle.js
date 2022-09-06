538b3cb2294fa4150c120480e2c58d45793c93a1
Florence-wangui <wanguif018@gmail.com>
2022-09-06 01:06:40 -0700

    Rectangle #1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Class Rectangle that defines a rectangle
+
+module.exports = class Rectangle {
+	constructor (width, height) {
+		this.width = width;
+		this.height = height;
+	}
+};
