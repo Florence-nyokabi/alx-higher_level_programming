0dc2340b7082b429fedc0bfc2ff83f23b208dcdf
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-07 09:57:10 +0300

    Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
-const list = require('./100-data').list;
-const newList = list.map((item, idx) => item * idx);
+const dict = require('./101-data').dict;
+const newDict = Object.entries(dict).reduce((item, [k, v]) => {
+  item[v] = item[v] ? [...item[v], k] : [k];
+  return item;
+}, {});
 
-console.log(list);
-console.log(newList);
+console.log(newDict);
