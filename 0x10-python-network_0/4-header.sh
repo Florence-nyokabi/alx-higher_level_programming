de5864d140615c612291cb598b5ebe6616e26e61
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-22 13:43:00 +0300

    cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#pass a variable in the header of the request
-curl -s "$1" -H "X-School-User-Id: 98"
+# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
