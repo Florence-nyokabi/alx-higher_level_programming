4ef5b3dea1ee26eaca8ca602597dc4431427c2c0
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-23 18:35:48 +0300

    Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""
-Use requests package to make a get request to given URL and display
-the value of `X-Request-Id` in response header.
-"""
-import sys
-import requests
+'''task 5 script'''
 
-if __name__ == "__main__":
-     url = sys.argv[1]
-     r = requests.get(url)
-     print(r.headers.get('X-Request-Id '))
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    res = requests.get(sys.argv[1])
+    print(res.headers.get('X-Request-Id'))
