b64cd1e59bed0e61319a43053881bed560df387a
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-23 18:20:06 +0300

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""
-Use requests package to make a post request to given URL with argument
-set in variable `q`, defaulting to empty string. If response body is properly
-JSON formatted and not empty, display `id` and `name` as given format.
-Otherwise display error message.
+"""Python script that takes in a letter and sends a
+POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
 """
 import sys
 import requests
 
+
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        arg = sys.argv[1]
-    else:
-        arg = ""
-        payload = {'q': arg}
-        url = "http://0.0.0.0:5000/search_user"
-        r = requests.post(url, data=payload)
-        try:
-            r.raise_for_status()
-            json = r.json()
-            if len(json) == 0:
-                print("No result")
-            else:
-                print("[{:d}] {}".format(json['id'], json['name']))
-        except Exception:
-            print("Not a valid JSON")
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    m = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = m.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
