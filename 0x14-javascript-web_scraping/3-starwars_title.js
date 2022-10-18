9a39f53b99c3fded913ad143639b5e2ae82ebfc2
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-10-18 17:43:45 +0300

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/';
-let episode = process.argv[2];
-request(url + episode, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    let jsonobj = JSON.parse(body);
-    console.log(jsonobj.title);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
-  }
-});
+
+const episodeId = process.argv[2];
+
+if (parseInt(episodeId) < 8) {
+  const url = 'https://swapi-api.hbtn.io/api/films/' + episodeId;
+
+  request(url, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(JSON.parse(body).title);
+  });
+}
