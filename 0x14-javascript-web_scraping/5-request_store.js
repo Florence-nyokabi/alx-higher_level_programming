d1c56240903f2f9b503557d533993f250297c7d1
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-10-18 17:40:44 +0300

    Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 request(url, function (err, response, body) {
-	if (err) {
-		console.log(err);
-	} else {
-		fs.writeFile(filePath, body, 'utf-8');
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8');
+  }
 });
