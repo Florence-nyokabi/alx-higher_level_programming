5d8365a598c338307ea8a7fd7f2d87df2dab4118
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-10-18 17:37:16 +0300

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request.get(url, (error, response, body) => {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log(`code: ${response.statusCode}`);
-	}
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
