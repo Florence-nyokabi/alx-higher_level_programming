eb0911e87d8b706b2ac955f68b51a060a9a5e5ff
Florence-wangui <wanguif018@gmail.com>
2022-10-18 06:51:21 -0700

    l

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+const stringWrite = process.argv[3];
+fs.writeFile(file, stringWrite, 'utf-8', (err) => {
+	if (err) console.log(err);
+});
