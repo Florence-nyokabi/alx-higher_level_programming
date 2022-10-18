fcfdf61cbb1535d1329aa3445c37c5ab741be98f
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-10-18 17:36:13 +0300

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 const file = process.argv[2];
-const stringWrite = process.argv[3];
-fs.writeFile(file, stringWrite, 'utf-8', (err) => {
-	if (err) console.log(err);
+const content = process.argv[3];
+fs.writeFile(file, content, 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
+  }
 });
